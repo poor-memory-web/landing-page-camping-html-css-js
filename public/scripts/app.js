e54317d95751a228834a9fc32455f1751338586a
Seyed Ehsan Hosseini <www.genshin.impact.41148@gmail.com>
2025-12-29 17:49:47 +0330

    coding mobile menu ---> css and js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,16 @@
 const navBtn = document.querySelector(".nav-btn");
+const mobileMenu = document.querySelector(".mobile-menu-login");
 
 let navOpen = false;
 
 navBtn.addEventListener("click", function () {
   if (navOpen == true) {
     navBtn.classList.remove("nav-btn--open");
+    mobileMenu.style.left = "-25rem";
     navOpen = false;
   } else {
     navBtn.classList.add("nav-btn--open");
+    mobileMenu.style.left = "0rem";
     navOpen = true;
   }
 });
