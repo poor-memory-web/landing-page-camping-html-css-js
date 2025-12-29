45f1bd4e2a5eedcbe8d05d24a1789f3745e2b22a
Seyed Ehsan Hosseini <www.genshin.impact.41148@gmail.com>
2025-12-29 23:52:45 +0330

    complete header nav

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,10 @@
 const navBtn = document.querySelector(".nav-btn");
 const mobileMenu = document.querySelector(".mobile-menu-login");
+const menuLinks = document.querySelectorAll(".menu__link");
 
 let navOpen = false;
 
-navBtn.addEventListener("click", function () {
+navBtn.addEventListener("click", () => {
   if (navOpen == true) {
     navBtn.classList.remove("nav-btn--open");
     mobileMenu.style.left = "-25rem";
@@ -13,4 +14,11 @@ navBtn.addEventListener("click", function () {
     mobileMenu.style.left = "0rem";
     navOpen = true;
   }
+});
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    menuLinks.forEach((l) => l.classList.remove("menu__link--active"));
+    link.classList.add("menu__link--active");
+  });
 });
